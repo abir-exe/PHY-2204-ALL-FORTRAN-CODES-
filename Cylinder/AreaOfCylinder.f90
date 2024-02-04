3c8c5076b05909d52b66d59c048d8af1c14b291a
Abir <abirmahmud221@gmail.com>
2024-02-05 00:44:51 +0600

    .

--- Cylinder/AreaOfCylinder.f90
+++ Cylinder/AreaOfCylinder.f90
@@ -6,6 +6,12 @@ PROGRAM areaOfCylinder
     READ*,R
     PRINT*,"ENTER THE VALUE OF HEIGHT(h):"
     READ*,H
+    if (R <= 0.0 .or. h <= 0.0) then
+    print*, "Invalid input. Both radius and height must be greater than zero."
+  else
     AREA = 2*PHI*R*(R+H)
     PRINT*,"THE AREA OF CYLINDER IS:", AREA
+  end if
+
+
 END PROGRAM
