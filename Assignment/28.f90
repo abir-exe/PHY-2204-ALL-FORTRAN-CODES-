4c32de906832ae91f0bcabd40498a09a355a1cfc
Abir <abirmahmud221@gmail.com>
2024-03-19 12:50:17 +0600

    CT-2 programs' are added, 28 of assignment and area-volume of cylinder

--- Assignment/28.f90
+++ Assignment/28.f90
@@ -1,1 +1,17 @@
 
+!1+2/3+3/3^2+4/3^3+.....+n/3^(n-1)
+
+program series_sum
+    implicit none
+    integer :: n, i
+    real :: sum=0.0
+
+    print*, "Enter the value of n:"
+    read*, n
+
+    do i = 1, n
+        sum = sum + i / 3.0**(i-1)
+    end do
+
+    print *, "The sum of the series is:", sum
+end program series_sum
