45f18f2329750b22f8ff6d3ba00ee930daba11a9
Abir <abirmahmud221@gmail.com>
2024-03-19 20:43:06 +0600

    number 35 added

--- Assignment/35.f90
+++ Assignment/35.f90
@@ -1,1 +1,16 @@
 
+program sum_of_series
+    implicit none
+    integer :: i
+    real :: sum_series
+
+    sum_series = 0.0
+
+    do i = 0, 8
+        sum_series = sum_series + 1.0 / 2.0**i
+    end do
+
+    print *, "Sum of the series is: ", sum_series
+
+
+end program sum_of_series
