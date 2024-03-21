d1663483d341aae4d745adb0d489cdca85f69917
Abir Mahmud <abirmahmud221@gmail.com>
2024-03-22 02:24:59 +0600

    Updated 36.f90

--- Assignment/36.f90
+++ Assignment/36.f90
@@ -1,13 +1,9 @@
 program sum
 implicit none
-real::s=0.0, i
-integer::n
-print*,"enter the number of terms"
-read*,n
-do i=1,n
-s=s+(0.5 *i)**2
+real::s=0.0
+integer::i
+do i=0,9
+s=s+((0.5 *i)+1)**2
 end do
 print*,"the required sum is=",s
 end program sum
-
-
